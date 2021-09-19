e6e7cec8213ea2457600204ca64230b2e4a70ff1
Kyle Buttress <kylebuttress11@gmail.com>
2021-09-19 14:32:24 +0200

    Removed action creator thunk to clean up file bulk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
 	name: 'cart',
@@ -40,55 +39,6 @@ const cartSlice = createSlice({
 		},
 	},
 });
-
-const firebaseURL =
-	'https://redux-cart-b1ac6-default-rtdb.europe-west1.firebasedatabase.app/cart.json';
-
-export const sendCartData = cart => {
-	return async dispatch => {
-		dispatch(
-			uiActions.showNotification({
-				status: 'pending',
-				title: 'Sending',
-				message: 'Sending cart data...',
-			})
-		);
-
-		// Updates our firebase cart whenever the cart is updated in the frontend
-		const sendRequest = async () => {
-		
-			const response = await fetch(firebaseURL, {
-				// PUT request overrides existing data in firebase DB
-				method: 'PUT',
-				body: JSON.stringify(cart),
-			});
-
-			if (!response.ok) {
-				throw new Error('Sending cart data has failed.');
-			}
-		};
-
-		try {
-			await sendRequest();
-
-			dispatch(
-				uiActions.showNotification({
-					status: 'success',
-					title: 'Success',
-					message: 'Sent cart data successfully!',
-				})
-			);
-		} catch (error) {
-			dispatch(
-				uiActions.showNotification({
-					status: 'error',
-					title: 'Error',
-					message: 'Sending cart data has failed.',
-				})
-			);
-		}
-	};
-};
 
 export const cartActions = cartSlice.actions;
 
