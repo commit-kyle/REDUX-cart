d1ccf7271d77d33301d8f57229a35eef17637747
Kyle Buttress <kylebuttress11@gmail.com>
2021-09-19 15:04:05 +0200

    Configured action creator thunk to handle fetching Firebase cart data on app start up

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,7 +1,38 @@
 import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
 
 const firebaseURL =
 	'https://redux-cart-b1ac6-default-rtdb.europe-west1.firebasedatabase.app/cart.json';
+
+export const fetchCartData = () => {
+  return async dispatch => {
+    const fetchData = async () => {
+      const response = await fetch(firebaseURL);
+
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data')
+      }
+
+      const data = await response.json();
+
+      return data;
+    }
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+				uiActions.showNotification({
+					status: 'error',
+					title: 'Error',
+					message: 'Fetching cart data has failed.',
+				})
+			);
+    }
+
+  }
+}
 
 export const sendCartData = cart => {
 	return async dispatch => {
